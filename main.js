6fa8286cf77881692bb097adff4abd99abf258ac
AstroTheNomer <50959906+AstroTheNomer@users.noreply.github.com>
2023-05-20 10:54:39 +0300

    nr + er support

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-let current_threat = 280
+let current_threat = 0
 let current_score = 0
 
+function setNormal() {
+    current_threat = 0
+    current_score = 0
+    updateXp()
+    $("#score").text(current_score)
+    $("#threat").text(current_threat)
+}
+function setExpert() {
+    current_threat = 280
+    current_score = 0
+    updateXp()
+    $("#score").text(current_score)
+    $("#threat").text(current_threat)
+}
+
 function addStep(baseXp) {
-    let xp = baseXp + (current_threat * (baseXp / 100))
+    let xp = baseXp + Math.round(current_threat * (baseXp / 100))
     current_score += xp
     current_threat += 5
     updateXp()
@@ -18,7 +33,9 @@ function addThreat(threat) {
 }
 
 function updateXp() {
-    $("#easy").text(`${80 + current_threat * 0.8}xp`)
-    $("#medium").text(`${100 + current_threat * 1}xp`)
-    $("#boss").text(`${150 + current_threat * 1.5}xp`)
+    $("#easy").text(`${80 + Math.round(current_threat * 0.8)}xp`)
+    $("#medium").text(`${100 + Math.round(current_threat * 1)}xp`)
+    $("#boss").text(`${150 + Math.round(current_threat * 1.5)}xp`)
 }
+
+updateXp()
